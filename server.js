ce77e5c37515ba17b07c2719597312d29dcb3954
Andrien <andrien26@gmail.com>
2026-09-13 19:41:30 +0100

    Add rate-limiting

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import { rateLimit } from 'express-rate-limit'
 import { albumsRouter } from './routes/albums.js'
 
 const PORT = 8000
 const app = express()
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100,
+	standardHeaders: 'draft-8', 
+	legacyHeaders: false,
+})
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors())
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 app.use('/api/albums', albumsRouter)
 
